957b3058c078f6eac5ce32e0c0a51092c0e8bf50
eliasnatti <enatti@gmail.com>
2026-05-27 14:47:53 +0000

    Deploying to gh-pages from @ eliasnatti/Smart-Architecture@2950f5cb88a745d2b5d3571285a6b0319373264e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "enatti-test/Smart-Architecture",
+    "data-repo": "eliasnatti/Smart-Architecture",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
