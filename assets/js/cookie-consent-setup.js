b140f5d40018fee90c2de1b7ca52e565200e0128
eliasnatti <eliasnatti@users.noreply.github.com>
2026-08-06 17:52:02 +0000

    Deploying to gh-pages from @ eliasnatti/Smart-Architecture@3dca9773cfb75524ce73d8fed2a5702c1503a988 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://enatti-test.github.io/Smart-Architecture/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://eliasnatti.github.io/Smart-Architecture/#contact">contact us</a>.'
               }
             ]
           }
